0d680e7b4cfa32465cd1f3c732f49d5482b5469d
Sthefanyom <om.sthefany@gmail.com>
2025-11-04 12:34:43 -0300

    Navbar, Footer e CardProduto

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,26 @@
+import {  GithubLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
+
+
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+
+    const data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-blue-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Quartzo Sollutions RH | Desenvolvido pelo Grupo 5 © {data}
+                        </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <div className='flex gap-2'>
+                        <LinkedinLogoIcon size={48} weight='bold' />
+                        <GithubLogoIcon size={48} weight='bold' />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Footer
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-blue-900 text-white'>
+            
+                <div className="container flex justify-between text-lg mx-8">
+                                        <Link 
+                        to='/home' 
+                        className='rounded-lg px-4 py-2 hover:bg-cyan-400 transform hover:scale-110 transition-transform duration-300 font-semibold text-xl'
+                    >
+                        Quartzo Sollutions RH
+                    </Link>
+
+                    <div className='flex gap-4'>
+                         <Link to ='/sobrenos' className=' rounded-lg px-4 py-2 hover:bg-cyan-400'>Sobre Nós</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
